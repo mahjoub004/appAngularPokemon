74d257dde18b2b8151352d02fb640d13590a0cc2
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2022-09-19 14:25:33 +0200

    creation des flux bidirectionnelle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,37 @@
 import { Component , OnInit } from '@angular/core';
-
+import { POKEMONS } from './mock-pokemon-list'
+import { Pokemon } from './pokemon';
 @Component({ //décorateur
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template : `<h1> Welcome to {{title}}</h1>`
+  //template : `<h1> Liste de Pokémons </h1>`
 })
 
 
 export class AppComponent implements OnInit{
-  title = ['fred' , 'monique' , 'rudy'];
+  ListPokemons : Pokemon[] = POKEMONS;
 
-  ngOnInit(): void {
+  pokemonSelected: Pokemon| undefined ;
 
-    console.table(this.title);
-
-    this.selectPokemon(this.title[2])
+  ngOnInit(): void { // methode lancer au moment  de chargement de la page
+    console.table(this.ListPokemons);
+    //this.selectPokemon(this.ListPokemons[1])
   }
 
-  selectPokemon(pokemonName: string){
-    console.log(`vous avez cliqué sur le pokemon ${pokemonName}`); // backtick
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon| undefined = this.ListPokemons
+    .find(pokemon => pokemon.id == +pokemonId)
+    if (pokemon) {
+      console.
+      log(`vous avez cliqué sur le pokemon ${pokemon.name}`); // backtick
+      this.pokemonSelected = pokemon ;
+    }else{
+      console.
+      log(`vous avez demander un pokemon qui n'existe pas}`);
+      this.pokemonSelected = pokemon ;
+    }
+
 
   }
 }
