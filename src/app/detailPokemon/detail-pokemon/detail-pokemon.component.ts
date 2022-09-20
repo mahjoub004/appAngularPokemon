ead77a58f63e92362bd932b5ca07db5a4e9eeb2c
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2022-09-20 14:38:16 +0200

    update detailPokemon component || comparer les deux ID (IdList and ID url )

--- src/app/detailPokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detailPokemon/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from 'src/app/mock-pokemon-list';
+import { Pokemon } from 'src/app/pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  listPokemon: Pokemon[]; // ma liste de pokemon
+  pokemon: Pokemon | undefined; // pokemon courant
 
-  ngOnInit(): void {
+  constructor( private route: ActivatedRoute) { } // avec < ActivatedRoute > rendre la service route disponible dans le composent
+
+  ngOnInit()  {
+    this.listPokemon = POKEMONS; // lier la liste pokemon
+
+      const pokemonId: string| null = this.route.snapshot.paramMap.get('id'); //recuperer l 'id dans l'URL
+      if (pokemonId) {
+        this.pokemon = this.listPokemon.find(pokemon => pokemon.id == +pokemonId) // comparer les deux ID de pokemon
+      }else{
+        this.pokemon = undefined ;
+      }
   }
 
 }
